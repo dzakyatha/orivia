ad0a0eecb841e9b41324bf74f93b374ead20c7d9
dzakyatha <18223124@std.stei.itb.ac.id>
2026-01-23 21:32:49 +0700

    feat: Update user role handling and full name assignment in serializers.py

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from .models import User, Profile
+from .models import User, Profile, UserRole
 from dj_rest_auth.registration.serializers import RegisterSerializer
 
 class ProfileSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='user.first_name', required=False, allow_blank=True)
+    
     class Meta:
         model = Profile
         fields = ['avatar_url', 'phone_number', 'full_name']
@@ -17,20 +19,19 @@ class CustomUserSerializer(serializers.ModelSerializer):
 class CustomRegisterSerializer(RegisterSerializer):
     username = None 
     full_name = serializers.CharField(required=True)
-    role = serializers.ChoiceField(choices=User.ROLE_CHOICES, default='customer')
+    role = serializers.ChoiceField(choices=UserRole.choices, default=UserRole.CUSTOMER)
     
     def get_cleaned_data(self):
         return {
             'email': self.validated_data.get('email', ''),
             'password1': self.validated_data.get('password1', ''),
             'full_name': self.validated_data.get('full_name', ''),
-            'role': self.validated_data.get('role', 'customer'),
+            'role': self.validated_data.get('role', UserRole.CUSTOMER),
         }
     
     def save(self, request):
         user = super().save(request)
-        user.role = self.validated_data.get('role', 'customer')
-        user.profile.full_name = self.validated_data.get('full_name', '')
+        user.role = self.validated_data.get('role', UserRole.CUSTOMER)
+        user.first_name = self.validated_data.get('full_name', '')
         user.save()
-        user.profile.save()
         return user
